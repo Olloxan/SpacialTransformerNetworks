2c24c9af60afa3d7c0527d8a2a9934db60d29113
Oliver Lange <oliver.lange@iwu.fraunhofer.de>
2019-10-29 11:38:41 +0100

    Added Classifier for numbers

--- Classify_MNIST.py
+++ Classify_MNIST.py
@@ -1,11 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
 import torch
-
+import torchvision
 from torchvision import datasets, transforms
 
 from Net import Net
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def test():
+    with torch.no_grad():
+        myNet.eval()
+        test_loss = 0
+        correct = 0
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = myNet(data)
+
+            # sum up batch loss
+            test_loss += F.nll_loss(output, target, size_average=False).item()
+            # get index of the max log-probability
+            pred = output.max(1, keepdim=True)[1]
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+        test_loss /= len(test_loader.dataset)
+        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'
+              .format(test_loss, correct, len(test_loader.dataset),
+                      100. * correct / len(test_loader.dataset)))
+
+def convert_image_np(inp):
+    """Convert a Tensor to numpy image."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    return inp
+
+def visualize_stn():
+    with torch.no_grad():
+        # Get a batch of training data
+        myNet.eval()
+        dataTuple = next(iter(test_loader)) 
+        data = dataTuple[0].to(device)
+        label = dataTuple[1]
+
+        input_tensor = data.cpu()
+        transformed_input_tensor = myNet.stn(data).cpu()
+
+        output = myNet(data)
+        
+        prediction = output.max(1, keepdim=True)[1]
+        
+        print(prediction.item())
+        
+        in_grid = convert_image_np(
+            torchvision.utils.make_grid(input_tensor))
+
+        out_grid = convert_image_np(
+            torchvision.utils.make_grid(transformed_input_tensor))
+
+        # Plot the results side-by-side
+        f, axarr = plt.subplots(1, 2)
+        axarr[0].imshow(in_grid)
+        axarr[0].set_title('Dataset Images')
+
+        axarr[1].imshow(out_grid)
+        axarr[1].set_title('Transformed Images')
 
 # Training Dataset
 if __name__ == '__main__':
@@ -13,6 +75,11 @@ if __name__ == '__main__':
         datasets.MNIST(root='.', train=False, transform=transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.1307,),(0.3081,))
-            ])), batch_size=64, shuffle=True, num_workers=4)
+            ])), batch_size=1, shuffle=True, num_workers=4)
 
      myNet = Net().to(device)
+     pretrained_dict = torch.load("Models/MNIST_Spacial", map_location='cpu')
+     myNet.load_state_dict(pretrained_dict)
+     
+     visualize_stn()
+     plt.show()
