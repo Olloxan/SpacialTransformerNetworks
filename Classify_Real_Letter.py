729d697dc791fc897e61313673191b8af2913bf9
Olloxan <axe999@gmx.de>
2019-11-11 11:18:35 +0100

    added histogram thresholding

--- Classify_Real_Letter.py
+++ Classify_Real_Letter.py
@@ -7,19 +7,36 @@ import torch
 
 from Net import Net_EMNIST
 
+#plt.ion()
+
 device = 'cpu' # torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 if __name__ == '__main__':
     
-    img = cv2.imread("TestData/a_1.jpg")
+    img = cv2.imread("TestData/A2.jpg")
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     plt.imshow(img, cmap="gray")
     plt.show()
 
-    test =    cv2.mean(img)
-    threshold = 128
-    img = 255 - img
+    test =    cv2.mean(img)   
+    img = 255 - img    
 
+    plt.imshow(img, cmap="gray")
+    plt.show()
+
+    hist, bins = np.histogram(img.flatten(),256, [0, 256])   
+    
+    for i in range(len(hist)):
+        x = hist[-i]
+        if x < 50:
+            threshold = 256 - i
+        if x > 50:
+            break
+
+    plt.hist(img.flatten(),256,[0,256])
+    plt.show()
+
+    
     _ , img = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
 
     plt.imshow(img, cmap="gray")
