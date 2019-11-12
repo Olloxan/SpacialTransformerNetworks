52e2e02a5524dbd4a289ad178c41c9697ba312cf
Olloxan <axe999@gmx.de>
2019-11-12 15:16:19 +0100

    moved functionality to extra class

--- Classify_Real_Letter.py
+++ Classify_Real_Letter.py
@@ -6,10 +6,11 @@ from torchvision import transforms
 import torch
 
 from Net import Net_EMNIST_3
-
-plt.ion()
+from Utilitiy.LetterPreprocessor import LetterPreprocessor
 
 device = 'cpu' # torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+processor = LetterPreprocessor()
 
 def showimg(image, title):
     plt.imshow(img, cmap="gray")
@@ -19,121 +20,8 @@ def showimg(image, title):
 if __name__ == '__main__':
     # input
     img = cv2.imread("TestData/1_1.jpg")
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    showimg(img, "input")
-    
-    # invert
-    img = 255 - img    
-    showimg(img, "inverting")
-
-    # histogram
-    hist, bins = np.histogram(img.flatten(),256, [0, 256])   
-    
-    for i in range(len(hist)):
-        x = hist[-i]
-        if x < 45:
-            threshold = 256 - i
-        if x > 45:
-            break
-        
-    plt.hist(img.flatten(),256,[0,256])
-    plt.title("threshold = %s" % threshold)
-    plt.show()
-
-    # thresholding
-    _ , img = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
-
-    showimg(img, "thresholding")
-
-    # opening
-    kernel = np.ones((5,5),np.uint8)
-    img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-    
-    showimg(img, "opening")
-    
-    # bounding box
-   
-    height = img.shape[0]
-    width = img.shape[1]
-    print("height = %s, width = %s" % (height, width))
-    padding = 4
-
-    # left
-    debugimg = img.copy()
-    left = width
-    for col in range(width):        
-        for row in range(height):                           
-            if img[row,col] > 0:
-                left = col
-                break
-            debugimg[row,col] = 128
-        if left < width:
-            break
-    left = max(0, left - padding)
-    print("left = %s" % left)
-
-    #plt.imshow(debugimg, cmap="gray")
-    #plt.show()
-   
-    # right
-    debugimg = img.copy()
-    right = 0
-    for col in reversed(range(width)):        
-        for row in range(height):                           
-            if img[row,col] > 0:
-                right = col
-                break
-            debugimg[row,col] = 128
-        if right > 0:
-            break
-    right = min(width, right + padding)
-    print("right = %s" % right)
-
-    #plt.imshow(debugimg, cmap="gray")
-    #plt.show()
-    
-    # top
-    debugimg = img.copy()
-    top = height
-    for row in range(height):   
-        for col in range(width):
-            if img[row,col] > 0:
-                top = row
-                break
-            debugimg[row,col] = 128
-        if top < height:
-            break
-    top = max(0,top - padding)
-    print("top = %s" % top)
-
-    #plt.imshow(debugimg, cmap="gray")
-    #plt.show()
-
-    # bottom
-    debugimg = img.copy()
-    bottom = 0
-    for row in reversed(range(height)):   
-        for col in range(width):
-            if img[row,col] > 0:
-                bottom = row
-                break
-            debugimg[row,col] = 128
-        if bottom > 0:
-            break
-    bottom = min(height,bottom + padding)
-    print("bottom = %s" % bottom)
-
-    #plt.imshow(debugimg, cmap="gray")
-    #plt.show()
-
-    # crop
-    img = img[top:bottom, left:right].copy()
-    showimg(img, "cropping")
-    plt.ioff()
-    # resize
-    img = cv2.resize(img, (28,28),interpolation=cv2.INTER_CUBIC)
-
-    showimg(img, "resizing")
+    img = processor.processImageShow(img)
+    #img = processor.processImage(img)
 
     myTransforms = transforms.Compose([
         transforms.ToTensor(),
