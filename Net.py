59a08b680dc5b955144fb5d928cb91b16dbdaba9
Oliver Lange <oliver.lange@iwu.fraunhofer.de>
2019-11-14 08:57:56 +0100

    merge commit

--- Net.py
+++ Net.py
@@ -56,12 +56,22 @@ class Net_MNIST(nn.Module):
 class Net_EMNIST(nn.Module):
     def __init__(self):
         super(Net_EMNIST, self).__init__()
-        self.conv1 = nn.Conv2d(1,10,kernel_size=5)
-        self.conv2 = nn.Conv2d(10,20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320,512)
-        self.fc2 = nn.Linear(512,100)
-        self.fc3 = nn.Linear(100,47)
+        self.conv = nn.Sequential(
+            nn.Conv2d(1,32, kernel_size=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Dropout(0.8),
+            nn.Conv2d(32,64,kernel_size=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Dropout(0.5))
+        
+        self.fc = nn.Sequential(
+            nn.Linear(1600,512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512,47),
+            nn.ReLU())        
 
         # Spatial transformer localization network
         self.loacalization = nn.Sequential(nn.Conv2d(1,8, kernel_size=7),
@@ -96,12 +106,7 @@ class Net_EMNIST(nn.Module):
         x = self.stn(x)
 
         # Perform the usual forward pass
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1,320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = F.relu(self.fc2(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc3(x)
+        x = self.conv(x) # F.relu(F.max_pool2d(self.conv1(x), 2))        
+        x = x.view(-1,1600)
+        x = self.fc(x)
         return F.log_softmax(x, dim=1)
